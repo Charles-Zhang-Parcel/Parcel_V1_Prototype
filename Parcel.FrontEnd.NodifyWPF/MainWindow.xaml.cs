6eebfe0628b3cbf43b7d60524c9d4979b1f47141
Charles Zhang <szinubuntu@gmail.com>
2022-04-01 01:56:29 -0700

    Prorotype GUI setup with Nodify

---
+++ Parcel.FrontEnd.NodifyWPF/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Parcel.FrontEnd.NodifyWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Public View Properties
+        public IEnumerable<string> Items { get; } = new List<string>
+        {
+            "My first item",
+            "My second item"
+        };
+        #endregion
+
+        #region Data Binding
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        private bool SetField<TType>(ref TType field, TType value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<TType>.Default.Equals(field, value)) return false;
+            field = value;
+            NotifyPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+
+        #region Events
+        private void MainWindow_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+                var cursor = Mouse.GetPosition(this);
+
+                PopupTab popupTab = new PopupTab()
+                {
+                    Left = this.Left + cursor.X,
+                    Top = this.Top + cursor.Y
+                };
+                popupTab.ShowDialog();
+            }
+        }
+        #endregion
+    }
+}
---
+++ Parcel.FrontEnd.NodifyWPF/PopupTab.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Parcel.FrontEnd.NodifyWPF
+{
+    public partial class PopupTab : Window
+    {
+        public PopupTab()
+        {
+            InitializeComponent();
+        }
+
+        private void PopupTab_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void PopupTab_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+                this.Close();
+        }
+    }
+}
