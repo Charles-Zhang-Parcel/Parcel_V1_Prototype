f41dabd8c569737625292f0c53f5864fd9ff7c48
Charles Zhang <szinubuntu@gmail.com>
2022-07-27 13:15:05 -0400

    [GUI Prototype] Fix Node Location Conversion
    - Fix binary formatter serialization for Net 6 (unsafe for web app)

---
+++ Parcel.FrontEnd.NodifyWPF/Converters/NodeLocationConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using Parcel.Shared.DataTypes;
+using Parcel.Shared.Framework.ViewModels.BaseNodes;
+
+namespace Parcel.FrontEnd.NodifyWPF.Converters
+{
+    public class NodeLocationConverter : IValueConverter
+    {
+        public object Convert(object source, Type targetType, object target, CultureInfo culture)
+        {
+            Vector2D loc = (Vector2D)source;
+            return new Point(loc.X, loc.Y);
+        }
+
+        public object ConvertBack(object source, Type targetType, object target, CultureInfo culture)
+        {
+            Point loc = (Point)source;
+            return new Vector2D(loc.X, loc.Y);
+        }
+    }
+}
